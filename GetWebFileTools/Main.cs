7874c8420f698e8276bd9971b339c7bebd9b665d
caihuamao520 <576403245@qq.com>
2020-02-18 21:31:45 +0800

    V1.0.0本开发完成，主要具备以下功能： 1、主要针对 智慧云职教（https://www.icve.com.cn/portal_new/portal/portal.html）网站的视频、PPT内容抓取下载地址。 2、抓取的地址自动复制到粘贴版，配合迅雷可以自动管理下载。

---
+++ GetWebFileTools/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GetWebFileTools
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            try
+            {
+                string url = this.webBrowser1.Document.ActiveElement.GetAttribute("href");
+
+                this.webBrowser1.Url = new Uri(url);
+            }
+            catch
+            {
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.txtURL.Text)) return;
+
+            this.webBrowser1.Url = new Uri(this.txtURL.Text); ;
+        }
+
+        private void btnGetViewDownURL_Click(object sender, EventArgs e)
+        {
+            HtmlElementCollection hec = this.webBrowser1.Document.Body.GetElementsByTagName("video");
+            if (hec.Count > 0)
+            {
+                this.txtViewDownURL.Text = hec[0].GetAttribute("src");
+
+                Clipboard.SetText(this.txtViewDownURL.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strOutHtml = this.webBrowser1.Document.Body.OuterHtml;
+            if (!string.IsNullOrEmpty(strOutHtml))
+            {
+                string regMatch = "<div\\sclass=\"docBox\"\\s.+downloadurl=\"(?<downUrl>.+)\\?response-content-disposition=.+;filename=(?<fileName>.+\\.ppt)\".+</div>";
+                Regex reg = new Regex(regMatch);
+                if (reg.IsMatch(strOutHtml))
+                {
+                    Match m = reg.Match(strOutHtml);
+                    if (m.Groups.Count > 0)
+                    {
+                        this.txtPPTDownURL.Text = m.Groups["downUrl"].Value;
+                        this.txtPPTName.Text = m.Groups["fileName"].Value;
+
+                        Clipboard.SetText(this.txtPPTDownURL.Text);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GetWebFileTools/Main.designer.cs
@@ -0,0 +1,218 @@
+﻿namespace GetWebFileTools
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnGetViewDownURL = new System.Windows.Forms.Button();
+            this.txtViewDownURL = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtPPTName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPPTDownURL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(0, 38);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(702, 590);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser1_NewWindow);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "网址：";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtURL.Location = new System.Drawing.Point(59, 6);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(840, 21);
+            this.txtURL.TabIndex = 2;
+            this.txtURL.Text = "https://www.icve.com.cn/portal_new/portal/portal.html";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(905, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGetViewDownURL
+            // 
+            this.btnGetViewDownURL.Location = new System.Drawing.Point(102, 117);
+            this.btnGetViewDownURL.Name = "btnGetViewDownURL";
+            this.btnGetViewDownURL.Size = new System.Drawing.Size(145, 38);
+            this.btnGetViewDownURL.TabIndex = 4;
+            this.btnGetViewDownURL.Text = "获取视频下载位置";
+            this.btnGetViewDownURL.UseVisualStyleBackColor = true;
+            this.btnGetViewDownURL.Click += new System.EventHandler(this.btnGetViewDownURL_Click);
+            // 
+            // txtViewDownURL
+            // 
+            this.txtViewDownURL.Location = new System.Drawing.Point(3, 28);
+            this.txtViewDownURL.Multiline = true;
+            this.txtViewDownURL.Name = "txtViewDownURL";
+            this.txtViewDownURL.ReadOnly = true;
+            this.txtViewDownURL.Size = new System.Drawing.Size(244, 83);
+            this.txtViewDownURL.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.txtPPTName);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtPPTDownURL);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtViewDownURL);
+            this.panel1.Controls.Add(this.btnGetViewDownURL);
+            this.panel1.Location = new System.Drawing.Point(708, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 590);
+            this.panel1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 38);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "获取PPT下载位置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtPPTName
+            // 
+            this.txtPPTName.Location = new System.Drawing.Point(5, 285);
+            this.txtPPTName.Name = "txtPPTName";
+            this.txtPPTName.ReadOnly = true;
+            this.txtPPTName.Size = new System.Drawing.Size(244, 21);
+            this.txtPPTName.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "PPT名称:";
+            // 
+            // txtPPTDownURL
+            // 
+            this.txtPPTDownURL.Location = new System.Drawing.Point(3, 184);
+            this.txtPPTDownURL.Multiline = true;
+            this.txtPPTDownURL.Name = "txtPPTDownURL";
+            this.txtPPTDownURL.ReadOnly = true;
+            this.txtPPTDownURL.Size = new System.Drawing.Size(244, 83);
+            this.txtPPTDownURL.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "PPT下载位置:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "视频下载位置:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 629);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "网页视频与PPT抓取工具";
+            this.Load += new System.EventHandler(this.Form6_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnGetViewDownURL;
+        private System.Windows.Forms.TextBox txtViewDownURL;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPPTName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPPTDownURL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
